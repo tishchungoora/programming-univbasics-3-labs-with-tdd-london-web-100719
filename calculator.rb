f56f92ccc98bcfb153695a726f8bfe27c27a79da
Tish Chungoora <tishal82@gmail.com>
2019-08-26 16:33:35 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
-# Add your variables here
+# Variables
+
+first_number = 9.81
+second_number = 10
+
+
+# Mathematical operations
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
+
